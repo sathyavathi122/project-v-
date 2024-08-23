a6d962dede669bcd43ad43cbbd8cdbbe867912a0
Ramesh <bobbadiramesh6@gmail.com>
2024-08-23 08:09:20 +0530

    homepage files from provject(v)

---
+++ HOMEPAGE.js
@@ -0,0 +1,20 @@
+function track(){
+    a=window.open("C:/Users/bobba/OneDrive/Desktop/projact(V)/TRACKPAGE.html")
+   
+    a.opener.close()
+
+}
+function profile(){
+    b=window.open("C:/Users/bobba/OneDrive/Desktop/projact(V)/profile.html")
+    b.opener.close()
+}
+function change(){
+document.getElementsByClassName("marquee")[0]. window.loction("C:/Users/bobba/OneDrive/Desktop/projact(V)/profile.html")
+}
+function marque(){
+   document.getElementsByClassName("marquee")[0].style.display="none"
+}
+function post(){
+    b=window.open("C:/Users/bobba/OneDrive/Desktop/projact(V)/POST.html")
+    b.opener.close()
+}
